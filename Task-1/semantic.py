b3eb5c735b152a33410b17145d7a82402460ad0e
Charles-Spencer Coleman <spencole@gmail.com>
2023-01-31 19:48:06 +0000

    added comments

--- Task-1/semantic.py
+++ Task-1/semantic.py
@@ -2,10 +2,12 @@ import spacy
 
 nlp = spacy.load('en_core_web_md')
 
+
 word1 = nlp("cat")
 word2 = nlp("monkey")
 word3 = nlp("banana")
 
+#display similarity
 print(word1.similarity(word2))
 print(word3.similarity(word2))
 print(word3.similarity(word1))
